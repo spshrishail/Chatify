df05302ac38f25ff434d9219d77904081b8f9e4d
spshrishail <161191083+spshrishail@users.noreply.github.com>
2025-02-25 20:58:02 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,14 +4,12 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    trim: true
+    required: true
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    trim: true,
     lowercase: true
   },
   password: {
