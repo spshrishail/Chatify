c425e281dbd846d2d84ec07bb2765220023e7872
spshrishail <161191083+spshrishail@users.noreply.github.com>
2025-02-25 19:42:25 +0530

    socketService.js

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000';
+const SOCKET_URL = 'https://chatify-theta-seven.vercel.app/';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
